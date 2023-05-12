10eba362824f4569604810edc07225e9d23131ae
Gavin Maynard <gmay8729@gmail.com>
2023-05-11 23:00:09 -0500

    enterd logo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 
 bg_color = "#404258"
 # #474E68
@@ -8,11 +9,13 @@ bg_color = "#404258"
 app = tk.Tk()
 app.title("Macro Panda")
 app.geometry("1024x576")
+app.iconbitmap("macro-panda-logo.ico")
 app.config(background=bg_color)
 
 header = tk.Label(text="Macro Panda",
                   font=("Times", 30),
                   background=bg_color,
                   foreground="white").pack(pady=(10, 0))
+
 
 app.mainloop()
